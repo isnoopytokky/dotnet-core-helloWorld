6bac438c0eb276187ba972683b280495c852129e
Pariwat Ramphuengnit <tokpower2@gmail.com>
2019-06-01 15:41:34 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,7 +8,51 @@ namespace HelloWeb
     {
         public static void Main(string[] args)
         {
+            
+            ///xxxxxxxxx
+            ///xxxxxxxxx
+            ///xxxxxxxxx
+            ///xxxxxxxxx
+            
+            ///xxxxxxxxx
+            ///xxxxxxxxx
+            ///xxxxxxxxx
+            ///xxxxxxxxx
+            
+            ///xxxxxxxxx
+            ///xxxxxxxxx
+            ///xxxxxxxxx
+            ///xxxxxxxxx
+            
+            ///xxxxxxxxx
+            ///xxxxxxxxx
+            ///xxxxxxxxx
+            ///xxxxxxxxx
+            
+            //var config = new ConfigurationBuilder()
+//                          .AddCommandLine(args)
+//                          .Build();
+//            var host = new WebHostBuilder()
+//                        .UseKestrel()
+//                        .UseConfiguration(config)
+//                        .UseContentRoot(Directory.GetCurrentDirectory())
+//                        .UseIISIntegration()
+//                        .UseStartup<Startup>()
+//                        .Build();
 
+//            host.Run();
+            //var config = new ConfigurationBuilder()
+//                          .AddCommandLine(args)
+//                          .Build();
+//            var host = new WebHostBuilder()
+//                        .UseKestrel()
+//                        .UseConfiguration(config)
+//                        .UseContentRoot(Directory.GetCurrentDirectory())
+//                        .UseIISIntegration()
+//                        .UseStartup<Startup>()
+//                        .Build();
+
+//            host.Run();
             var config = new ConfigurationBuilder()
                           .AddCommandLine(args)
                           .Build();
